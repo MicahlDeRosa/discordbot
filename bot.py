c95e6988491dd08ae6d6ededd8552997a325d325
MicahlDeRosa <derosam14@gmail.com>
2021-08-18 09:46:54 -0400

    added 8ball command and added a ban and kick command

--- bot.py
+++ bot.py
@@ -1,5 +1,6 @@
 import os
 import discord
+import random
 from discord.ext import commands
 from dotenv import load_dotenv
 
@@ -15,6 +16,37 @@ async def on_ready():
 async def ping(ctx):
     await ctx.send('!Pong')
 
+
+@client.command(aliases=['8b'])
+async def eightball(ctx,*,question):
+    responses = [
+        'Hell no.',
+        'Prolly not.',
+        'Idk bro.',
+        'Prolly.',
+        'Hell yeah my dude.',
+        'It is certain.',
+        'It is decidedly so.',
+        'Without a Doubt.',
+        'Yes - Definitaly.',
+        'You may rely on it.',
+        'As i see it, Yes.',
+        'Most Likely.',
+        'Outlook Good.',
+        'Yes!',
+        'No!',
+        'Signs a point to Yes!',
+        'Reply Hazy, Try again.',
+        'IDK but u should subscribe to Exotix On Youtube and Follow Him On Insta mahad.ali.khan.',
+        'Better not tell you know.',
+        'Cannot predict now.',
+        'Concentrate and ask again.',
+        "Don't Count on it.",
+        'My reply is No.',
+        'My sources say No.',
+        'Outlook not so good.',
+        'Very Doubtful']
+    await ctx.send(f':8ball: Question: {question}\n:8ball: Answer: {random.choice(responses)} ')
 
 load_dotenv()
 client.run(os.getenv('TOKEN'))
