242c2fabec78025422243b35f7634ab417389c5c
MicahlDeRosa <derosam14@gmail.com>
2021-08-18 10:18:13 -0400

    added as delete messages commad

--- bot.py
+++ bot.py
@@ -51,17 +51,26 @@ async def eightball(ctx,*,question):
 
 @client.command(aliases=['boot'])
 async def kick(ctx, member:discord.Member,*, reason=None):
+    if (not ctx.author.guild_permission.kick_members):
+        await ctx.send('This command requires ``Ban Members``')
+        return
     await member.kick(reason=reason)
     await ctx.send(f'{member.mention} has been kicked')
 
 @client.command(aliases=['banned'])
 async def ban(ctx, member:discord.Member,*, reason=None):
+    if (not ctx.author.guild_permission.ban_members):
+        await ctx.send('This command requires ``Kick Members``')
+        return
     await member.ban(reason=reason)
     await ctx.send(f'{member.mention} has been banned')
 
 
 @client.command(aliases=['forgive']) 
 async def unban(ctx,*, member):
+    if (not ctx.author.guild_permission.ban_members):
+        await ctx.send('This command requires ``Ban Members``')
+        return
     banned_users = await ctx.guild.bans()
     member_name, member_discriminator = member.split('#')
 
@@ -72,6 +81,24 @@ async def unban(ctx,*, member):
             await ctx.guild.unban(user)
             await ctx.send(f'Unbannerd {user.mention}')
             return
+
+
+@client.command(aliases=['purge']) 
+async def clear(ctx, amount=11):
+    if (not ctx.author.guild_permission.manage_messages):
+        await ctx.send('This command requires ``Manage Messages``')
+        return
+    if amount > 101:
+        await ctx.send('Can not delete more than 100 messages')
+    else:
+        await ctx.channel.purge(limit=amount)
+        await ctx.send('Cleared Messages')
+
+
+
+
+
+
 
 load_dotenv()
 client.run(os.getenv('TOKEN'))
