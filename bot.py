f4c713755a2cd460e330426374d6d77ab828f4ae
MicahlDeRosa <derosam14@gmail.com>
2021-08-18 10:02:38 -0400

    added a unban

--- bot.py
+++ bot.py
@@ -48,6 +48,31 @@ async def eightball(ctx,*,question):
         'Very Doubtful']
     await ctx.send(f':8ball: Question: {question}\n:8ball: Answer: {random.choice(responses)} ')
 
+
+@client.command(aliases=['boot'])
+async def kick(ctx, member:discord.Member,*, reason=None):
+    await member.kick(reason=reason)
+    await ctx.send(f'{member.mention} has been kicked')
+
+@client.command(aliases=['banned'])
+async def ban(ctx, member:discord.Member,*, reason=None):
+    await member.ban(reason=reason)
+    await ctx.send(f'{member.mention} has been banned')
+
+
+@client.command(aliases=['forgive']) 
+async def unban(ctx,*, member):
+    banned_users = await ctx.guild.bans()
+    member_name, member_discriminator = member.split('#')
+
+    for ban_entry in banned_users:
+        user = ban_entry.user
+
+        if(user.name, user.discriminator) == (member_name, member_discriminator):
+            await ctx.guild.unban(user)
+            await ctx.send(f'Unbannerd {user.mention}')
+            return
+
 load_dotenv()
 client.run(os.getenv('TOKEN'))
 
